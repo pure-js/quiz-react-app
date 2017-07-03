e508923674c2a0cf2a4e4c8856a6e3bb5c7fd92d
pure-js <evolution.is.already.underway@gmail.com>
2017-07-04 00:25:30 +0300

    Up gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -10,45 +10,13 @@ const gulp = require('gulp'),
 const clean = () => del([ paths.build, paths.dev, paths.deploy ]);
 exports.clean = clean;
 
-// Grab js files and save as html with tags for hightlighting
-function wrap() {
-  const folder = './exercises/';
-  const tmpFolder = '.tmp/';
-  const fs = require('fs');
-
-  mkdirp(tmpFolder, function(err) {
-    if (err) throw err;
-  });
-
-  fs.readdir(folder, (err, files) => {
-    files.forEach(file => {
-      if(file.indexOf('.') !== -1) {
-        let contents = fs.readFileSync(folder + file, 'utf8');
-
-        let html = highlighter.highlightSync({
-          fileContents: contents,
-          scopeName: 'source.js'
-        });
-
-        let htmlFile = file.replace(/\.[^/.]+$/, '.html');
-
-        fs.writeFile(tmpFolder + htmlFile, html, (err) => {
-          if (err) throw err;
-          console.log(htmlFile + ' is saved!');
-        });
-      }
-    });
-  });
-}
-
 // The default task (called when you run `gulp` from cli)
-const dev = () => gulp.parallel(wrap, develop);
-exports.dev = dev;
+exports.dev = develop;
+exports.build = build;
 
-gulp.task('build', gulp.parallel(build));
-exports.default = dev;
+exports.default = develop;
 
-gulp.task('deploy', gulp.series('build', () =>
+gulp.task('deploy', gulp.series(build, () =>
   gulp.src(paths.build + '**/*')
     .pipe(ghPages())
 ));
