f0e902aa3bd1ca79a1da83469bbc9dee0c779572
pure-js <evolution.is.already.underway@gmail.com>
2017-12-20 00:26:08 +0300

    Add rect linting

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-// import addRow from './components/textarea/textAreaAddRow';
-// import showNextQuiz from './components/showNextQuiz';
-// import questions from '../static/questions';
-// import current from './components/current';
-// import domElements from './components/domElements';
-// import shuffleArray from './components/shuffleArray';
-// import toAnswer from './components/toAnswer';
+import addRow from './components/textarea/textAreaAddRow';
+import showNextQuiz from './components/showNextQuiz';
+import questions from '../static/questions';
+import current from './components/current';
+import domElements from './components/domElements';
+import shuffleArray from './components/shuffleArray';
+import toAnswer from './components/toAnswer';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -14,34 +14,34 @@ ReactDOM.render(
   document.getElementById('app')
 );
 
-// window.addEventListener('load', () => {
-//   domElements.$exam.addEventListener('click', () => {
-//     current.quiz = shuffleArray(questions);
-//     showNextQuiz(questions);
-//     document.getElementById('first-screen').classList.add('d-none');
-//     document.getElementById('quiz-screen').classList.remove('d-none');
-//     domElements.$exam.removeEventListener('click', () => {});
-//   });
+window.addEventListener('load', () => {
+  domElements.$exam.addEventListener('click', () => {
+    current.quiz = shuffleArray(questions);
+    showNextQuiz(questions);
+    document.getElementById('first-screen').classList.add('d-none');
+    document.getElementById('quiz-screen').classList.remove('d-none');
+    domElements.$exam.removeEventListener('click', () => {});
+  });
 
-//   domElements.$nextQuiz.addEventListener('click', () => {
-//     showNextQuiz(questions);
-//   });
+  domElements.$nextQuiz.addEventListener('click', () => {
+    showNextQuiz(questions);
+  });
 
-//   domElements.$textArea.addEventListener('keypress', () => {
-//     addRow('console-output');
-//   });
+  domElements.$textArea.addEventListener('keypress', () => {
+    addRow('console-output');
+  });
 
-//   domElements.$answer.addEventListener('click', () => {
-//     toAnswer();
-//   });
+  domElements.$answer.addEventListener('click', () => {
+    toAnswer();
+  });
 
-//   if ('serviceWorker' in navigator) {
-//     navigator.serviceWorker.register('sw.min.js')
-//       .then((registration) => {
-//         console.log('Registered:', registration);
-//       })
-//       .catch((error) => {
-//         console.log('Registration failed: ', error);
-//       });
-//   }
-// });
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.min.js')
+      .then((registration) => {
+        console.log('Registered:', registration);
+      })
+      .catch((error) => {
+        console.log('Registration failed: ', error);
+      });
+  }
+});
