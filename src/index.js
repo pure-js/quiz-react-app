159f66e27a7548bfdac4e1f7010cc886f6fc68f2
pure-js <evolution.is.already.underway@gmail.com>
2017-12-20 00:18:21 +0300

    Add react

--- src/index.js
+++ src/index.js
@@ -1,39 +1,47 @@
-import addRow from './components/textarea/textAreaAddRow';
-import showNextQuiz from './components/showNextQuiz';
-import questions from '../static/questions';
-import current from './components/current';
-import domElements from './components/domElements';
-import shuffleArray from './components/shuffleArray';
-import toAnswer from './components/toAnswer';
+// import addRow from './components/textarea/textAreaAddRow';
+// import showNextQuiz from './components/showNextQuiz';
+// import questions from '../static/questions';
+// import current from './components/current';
+// import domElements from './components/domElements';
+// import shuffleArray from './components/shuffleArray';
+// import toAnswer from './components/toAnswer';
 
-window.addEventListener('load', () => {
-  domElements.$exam.addEventListener('click', () => {
-    current.quiz = shuffleArray(questions);
-    showNextQuiz(questions);
-    document.getElementById('first-screen').classList.add('d-none');
-    document.getElementById('quiz-screen').classList.remove('d-none');
-    domElements.$exam.removeEventListener('click', () => {});
-  });
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-  domElements.$nextQuiz.addEventListener('click', () => {
-    showNextQuiz(questions);
-  });
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('app')
+);
 
-  domElements.$textArea.addEventListener('keypress', () => {
-    addRow('console-output');
-  });
+// window.addEventListener('load', () => {
+//   domElements.$exam.addEventListener('click', () => {
+//     current.quiz = shuffleArray(questions);
+//     showNextQuiz(questions);
+//     document.getElementById('first-screen').classList.add('d-none');
+//     document.getElementById('quiz-screen').classList.remove('d-none');
+//     domElements.$exam.removeEventListener('click', () => {});
+//   });
 
-  domElements.$answer.addEventListener('click', () => {
-    toAnswer();
-  });
+//   domElements.$nextQuiz.addEventListener('click', () => {
+//     showNextQuiz(questions);
+//   });
 
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.min.js')
-      .then((registration) => {
-        console.log('Registered:', registration);
-      })
-      .catch((error) => {
-        console.log('Registration failed: ', error);
-      });
-  }
-});
+//   domElements.$textArea.addEventListener('keypress', () => {
+//     addRow('console-output');
+//   });
+
+//   domElements.$answer.addEventListener('click', () => {
+//     toAnswer();
+//   });
+
+//   if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker.register('sw.min.js')
+//       .then((registration) => {
+//         console.log('Registered:', registration);
+//       })
+//       .catch((error) => {
+//         console.log('Registration failed: ', error);
+//       });
+//   }
+// });
