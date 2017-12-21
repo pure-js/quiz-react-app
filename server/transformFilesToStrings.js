c55b51909f8e681f138d79f0622bef490dfa3a35
pure-js <evolution.is.already.underway@gmail.com>
2017-12-21 23:05:35 +0300

    improve node script

--- server/transformFilesToStrings.js
+++ server/transformFilesToStrings.js
@@ -1,23 +1,16 @@
 const fs = require('fs');
+const recursive = require('recursive-readdir');
 
 const param = process.argv.slice(2)[0];
 const folderName = 'exercises';
 
-function getFilesList(folder) {
-  return new Promise((resolve, reject) => {
-    fs.readdir(folder, (err, files) => {
-      if (err) reject(err);
-      resolve(files);
-    });
-  });
-}
-
-function getFileContent(name, file) {
+function getFileContent(name, category, file) {
   return new Promise((resolve, reject) => {
     fs.readFile(file, 'utf8', (err, data) => {
       if (err) reject(err);
       resolve({
         name,
+        category,
         value: data,
       });
     });
@@ -28,25 +21,28 @@ function getFilesContent(folder, files) {
   const arr = [];
   const { length } = files;
   for (let i = 0; i < length; i += 1) {
-    const file = folder + '/' + files[i];
-
-    if (file.slice(-2) === 'js') {
-      arr.push(getFileContent(files[i].slice(0, -3), file));
+    const file = files[i];
+    const index = file.indexOf('\\');
+    const withoutParentFolder = file.substring(index + 1);
+    let category = 'unclassified';
+    if (withoutParentFolder.indexOf('\\') >= 0) {
+      category = withoutParentFolder.substring(0, withoutParentFolder.indexOf('\\'));
     }
+    const fileName = withoutParentFolder.slice(0, -3);
+    arr.push(getFileContent(fileName, category, file));
   }
 
   return arr;
 }
 
-getFilesList(folderName).then((files) => {
-  const zo = getFilesContent(folderName, files);
-  return Promise.all(zo);
-}).then((content) => {
-  const paramFolder = param.substring(0, param.indexOf('/'));
+recursive(folderName)
+  .then(files => Promise.all(getFilesContent(folderName, files)))
+  .then((content) => {
+    const paramFolder = param.substring(0, param.indexOf('/'));
 
-  fs.mkdir(paramFolder, () => {
-    fs.writeFile(param, 'const questions = ' + JSON.stringify(content, null, 2) + ';\nexport default questions;\n', 'utf8', (err) => {
-      if (err) throw err;
+    fs.mkdir(paramFolder, () => {
+      fs.writeFile(param, 'const questions = ' + JSON.stringify(content, null, 2) + ';\nexport default questions;\n', 'utf8', (err) => {
+        if (err) throw err;
+      });
     });
   });
-});
