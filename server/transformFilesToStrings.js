0564876c047423c0cd0d707d12ed8360b9d71833
pure-js <evolution.is.already.underway@gmail.com>
2017-12-22 18:26:01 +0300

    improve node script for parsing questions

--- server/transformFilesToStrings.js
+++ server/transformFilesToStrings.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
+const util = require('util');
 const recursive = require('recursive-readdir');
 
 const param = process.argv.slice(2)[0];
-const folderName = 'exercises';
+const folderName = 'questions';
 
 function getFileContent(name, category, file) {
   return new Promise((resolve, reject) => {
@@ -22,13 +23,15 @@ function getFilesContent(folder, files) {
   const { length } = files;
   for (let i = 0; i < length; i += 1) {
     const file = files[i];
-    const index = file.indexOf('\\');
-    const withoutParentFolder = file.substring(index + 1);
+    let index = file.indexOf('\\');
+    let withoutFolder = file.substring(index + 1);
     let category = 'unclassified';
-    if (withoutParentFolder.indexOf('\\') >= 0) {
-      category = withoutParentFolder.substring(0, withoutParentFolder.indexOf('\\'));
+    index = withoutFolder.indexOf('\\');
+    if (index >= 0) {
+      category = withoutFolder.substring(0, index);
+      withoutFolder = withoutFolder.substring(index + 1);
     }
-    const fileName = withoutParentFolder.slice(0, -3);
+    const fileName = withoutFolder.slice(0, -3);
     arr.push(getFileContent(fileName, category, file));
   }
 
@@ -41,7 +44,7 @@ recursive(folderName)
     const paramFolder = param.substring(0, param.indexOf('/'));
 
     fs.mkdir(paramFolder, () => {
-      fs.writeFile(param, 'const questions = ' + JSON.stringify(content, null, 2) + ';\nexport default questions;\n', 'utf8', (err) => {
+      fs.writeFile(param, `const questions =\n${util.inspect(content)};\n\nexport default questions;\n`, 'utf8', (err) => {
         if (err) throw err;
       });
     });
--- static/answers.js
+++ static/answers.js
@@ -1,30 +1,60 @@
-const answers = [{
-  name: "10",
-  value: '4',
-}, {
-  name: "11",
-  value: "undefined",
-}, {
-  name: "12",
-  value: "true",
-}, {
-  name: "13",
-  value: "false",
-}, {
-  name: '4',
-  value : '1object',
-}, {
-  name: '6',
-  value : "NaN",
-}, {
-  name: '7',
-  value : "NaN",
-}, {
-  name: '8',
-  value : '8',
-}, {
-  name: '9',
-  value : "0 1",
-}];
+const answers = [
+  {
+    name: '10',
+    value: '4',
+  },
+  {
+    name: '11',
+    value: 'undefined',
+  },
+  {
+    name: '12',
+    value: 'true',
+  },
+  {
+    name: '13',
+    value: 'false',
+  },
+  {
+    name: '6',
+    value: 'NaN',
+  },
+  {
+    name: '4',
+    value: '1object',
+  },
+  {
+    name: '7',
+    value: 'NaN',
+  },
+  {
+    name: '8',
+    value: '8',
+  },
+  {
+    name: '9',
+    value: '0 1',
+  },
+  {
+    name: '1',
+    value: '10',
+  },
+  {
+    name: '2',
+    value: '1\n3',
+  },
+  {
+    name: '3',
+    value: '1',
+  },
+  {
+    name: 'two-for-loops',
+    value: '10\n10\n10\n10\n10\n10\n10\n10\n10\n10',
+  },
+  {
+    name: '5',
+    value: 'undefined',
+  },
+];
 
 export default answers;
