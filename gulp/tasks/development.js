d20ac06b384f906a3cb85540d893d85f7d19c9d0
pure-js <evolution.is.already.underway@gmail.com>
2017-12-25 13:17:47 +0300

    remove unused dependencies

--- gulp/tasks/development.js
+++ gulp/tasks/development.js
@@ -1,25 +1,9 @@
 import gulp from 'gulp';
-import gulpLoadPlugins from 'gulp-load-plugins';
 import { development } from '../config';
-import webpack from 'webpack-stream';
-const webpackConfig = require('../../webpack.dev.js');
-
-const plugins = gulpLoadPlugins();
-
-const js = () =>
-  gulp.src(development.js)
-    .pipe(plugins.plumber())
-    .pipe(webpack(webpackConfig))
-    .pipe(gulp.dest(development.dest));
 
 const copy = () =>
   gulp.src(development.copy)
     .pipe(gulp.dest(development.dest));
 
-// Rerun the task when a file changes
-const watch = () => {
-  gulp.watch(development.jsWatch, js);
-};
-
-const develop = gulp.series(gulp.parallel(js, copy), watch);
+const develop = gulp.parallel(copy);
 export default develop;
