53c241dc1098d3142010d7d2920cf59b074eba45
pure-js <evolution.is.already.underway@gmail.com>
2017-12-25 13:19:20 +0300

    Home page is ready for using css modules

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 import bootstrap from '../../node_modules/bootstrap/dist/css/bootstrap.css';
 import styles from './Home.css';
@@ -11,21 +12,21 @@ const Home = (props) => {
     <div>
       <div className={styles['arrow-right']} />
       <main className={bootstrap.container}>
-        <div id="first-screen" className="row">
-          <div className="col-12">
-            <div className="d-flex justify-content-center">
-              <div className="btn-group">
+        <div id="first-screen" className={bootstrap.row}>
+          <div className={bootstrap['col-12']}>
+            <div className={classNames(bootstrap['d-flex'], bootstrap['justify-content-center'])}>
+              <div className={bootstrap['btn-group']}>
                 <button
                   id="exam"
                   type="button"
-                  className="btn btn-lg btn-info btn_start"
+                  className={classNames(bootstrap.btn, bootstrap['btn-lg'], bootstrap['btn-info'], styles.btn_start, styles.btn_cursor)}
                   onClick={action}
                 >Exam
                 </button>
                 <button
                   id="train"
                   type="button"
-                  className="btn btn-lg btn-info btn_start"
+                  className={classNames(bootstrap.btn, bootstrap['btn-lg'], bootstrap['btn-info'], styles.btn_start, styles.btn_cursor)}
                   disabled
                 >Train
                 </button>
