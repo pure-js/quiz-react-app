8845d842d05cee653d54240c8cac5fbbffcf0123
pure-js <evolution.is.already.underway@gmail.com>
2017-12-27 18:07:21 +0300

    refactor

--- webpack.dev.babel.js
+++ webpack.dev.babel.js
@@ -4,64 +4,68 @@ import ScriptExtHtmlWebpackPlugin from 'script-ext-html-webpack-plugin';
 import webpack from 'webpack';
 import CopyWebpackPlugin from 'copy-webpack-plugin';
 
+const plugins = [
+  new CopyWebpackPlugin([
+    {
+      from: 'manifest.json',
+    },
+  ]),
+  new HtmlWebpackPlugin({
+    template: 'src/index-template.html',
+  }),
+  new ScriptExtHtmlWebpackPlugin({
+    defaultAttribute: 'defer',
+  }),
+  new webpack.NamedModulesPlugin(),
+  new webpack.HotModuleReplacementPlugin(),
+];
+
+const module = {
+  rules: [
+    {
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            '@babel/preset-react',
+            '@babel/preset-env',
+          ],
+        },
+      },
+    },
+    {
+      test: /\.css$/,
+      use: [
+        {
+          loader: 'style-loader',
+        },
+        {
+          loader: 'css-loader',
+          options: {
+            modules: true,
+          },
+        },
+      ],
+    },
+  ],
+};
+
 const config = {
   entry: {
     app: './src/index.js',
   },
-  plugins: [
-    new CopyWebpackPlugin([
-      {
-        from: 'manifest.json',
-      },
-    ]),
-    new HtmlWebpackPlugin({
-      template: 'src/index-template.html',
-    }),
-    new ScriptExtHtmlWebpackPlugin({
-      defaultAttribute: 'defer',
-    }),
-    new webpack.NamedModulesPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-  ],
+  plugins,
+  module,
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, '.tmp'),
   },
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: '.tmp',
+    contentBase: 'dist',
     hot: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/preset-react',
-              '@babel/preset-env',
-            ],
-          },
-        },
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: 'style-loader',
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-            },
-          },
-        ],
-      },
-    ],
   },
 };
 
