fc6f4e594386f95ffb5518807d37ca39f7824a16
pure-js <evolution.is.already.underway@gmail.com>
2018-01-19 16:50:15 +0300

    fix path

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').then((registration) => {
+    navigator.serviceWorker.register('sw.js').then((registration) => {
       console.log('SW registered: ', registration);
     }).catch((registrationError) => {
       console.log('SW registration failed: ', registrationError);
