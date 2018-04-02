765ce379f90233e5e3b36d96891ba3095f4af945
pure-js <3690368+pure-js@users.noreply.github.com>
2018-04-03 02:32:50 +0300

    add provider for react

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
+import rootReducer from './reducers';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
