e0a572a60999804f06ef5a7371078d5a80daf492
pure-js <3690368+pure-js@users.noreply.github.com>
2018-04-27 13:12:37 +0300

    make shuffleArray a pre function

--- src/services/shuffleArray.js
+++ src/services/shuffleArray.js
@@ -1,12 +1,13 @@
-const getRand = i => Math.floor(Math.random() * (i + 1));
+const getRand = max => Math.floor(Math.random() * (max + 1));
 
 function shuffleArray(arr) {
-  const newArr = arr;
-  for (let i = newArr.length - 1; i > 0; i -= 1) {
+  const shuffled = [...arr];
+  const { length } = shuffled;
+  for (let i = length - 1; i > 0; i -= 1) {
     const j = getRand(i);
-    [newArr[i], newArr[j]] = [newArr[j], newArr[i]];
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
   }
-  return newArr;
+  return shuffled;
 }
 
 export default shuffleArray;
