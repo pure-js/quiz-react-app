87670115fbd4b35bf8aae95fc709096e3d41b61b
pure-js <3690368+pure-js@users.noreply.github.com>
2019-01-25 23:25:49 +0300

    rename GetRand to floatToInteger

--- src/services/shuffleArray.js
+++ src/services/shuffleArray.js
@@ -1,11 +1,11 @@
 // @flow
-import getRand from './floatToInteger';
+import floatToInteger from './floatToInteger';
 
 function shuffleArray(arr: Array<any>) {
   const shuffled = arr.slice();
   const { length } = shuffled;
   for (let i = length - 1; i > 0; i -= 1) {
-    const j = getRand(i);
+    const j = floatToInteger(i);
     [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
   }
   return shuffled;
