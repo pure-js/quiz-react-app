2c4d93072b7fa8c491e09514e484b8132e18ba94
pure-js <3690368+pure-js@users.noreply.github.com>
2019-01-30 01:29:54 +0300

    remove commented code

--- src/scenes/Exam/Exam.jsx
+++ src/scenes/Exam/Exam.jsx
@@ -91,7 +91,8 @@ class Exam extends Component<Props, State> {
 
   addAnswer = (answer: string): void => {
     const isCorrectAnswer = functions.httpsCallable('isCorrectAnswer');
-    isCorrectAnswer()
+    console.log(answer, 'answer');
+    isCorrectAnswer(answer)
       .then((result) => {
         this.displayQuestion();
         // Read result of the Cloud Function.
@@ -99,52 +100,18 @@ class Exam extends Component<Props, State> {
       });
   };
 
-  handleAnyAnswer = () => {
-    // if (this.iteration < this.maxIteration) {
-    //   this.userAnswers.push({
-    //     name: this.state.question.name,
-    //     answer: this.userAnswer,
-    //   });
-    //   this.iteration = this.iteration + 1;
-    //   this.setState({
-    //     question: this.questions[this.iteration],
-    //   });
-    //   this.answer = answers.find(answer => answer.name === this.state.question.name);
-    //   alert(this.answer.value);
-    // } else if (this.state.question === this.questions[this.questionsLength - 1]) {
-    //   this.props.results();
-    // }
-  };
-
-  handleAnswer = () => {
-    // fetch('https://us-central1-js-quiz-31f79.cloudfunctions.net/isCorrectAnswer')
-    //   .then(response => response.json())
-    //   .then((myJson) => {
-    //     console.log(myJson);
-    //   });
-
-    // if (Exam.answerIsCorrect(this.userAnswer, this.answer.value)) {
-    //   this.successCounter += 1;
-    //   const percent = (this.successCounter * 100) / this.questionsLength;
-    //   this.success = {
-    //     width: `${percent}%`,
-    //   };
-    //   this.handleAnyAnswer();
-    // } else {
-    //   this.handleNotAnswer();
-    // }
-  };
-
   render() {
+    const { home } = this.props;
+    const { question } = this.state;
     return (
       <div>
-        <Header home={this.props.home} current={this.iteration + 1} total={this.questionsLength} />
+        <Header home={home} current={this.iteration + 1} total={this.questionsLength} />
         <ProgressBar success={this.success} failure={this.failure} overall={this.questionsLength} />
         <section className={classNames(grid.container, grid['container_mobile-no-padding'])}>
-          <LoadableCode question={this.state.question.value} />
+          <LoadableCode question={question.value} />
         </section>
         <section className={grid.container}>
-          <Form userAnswer={this.addAnswer} handleAnswer={this.handleAnswer} />
+          <Form userAnswer={this.addAnswer} />
         </section>
       </div>
     );
