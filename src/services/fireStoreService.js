18f6cb345d8415d49da7162a65d3e342238c0d6d
pure-js <3690368+pure-js@users.noreply.github.com>
2019-01-30 23:37:58 +0300

    remove commented code

--- src/services/fireStoreService.js
+++ src/services/fireStoreService.js
@@ -4,15 +4,6 @@ import 'firebase/functions';
 import firebaseService from './firebaseService';
 
 const firestore = firebaseService.firestore();
-// firestore().enablePersistence()
-//   .catch((err) => {
-//     if (err.code === 'failed-precondition') {
-//       console.error('Multiple tabs open, persistence can only be enabled in one tab at a a time.');
-//     } else if (err.code === 'unimplemented') {
-//       console.error('The current browser does not support all of the features required to enable persistence');
-//     }
-//   });
-
 const functions = firebaseService.functions('europe-west1');
 
 export {
