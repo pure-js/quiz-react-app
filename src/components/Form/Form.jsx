5d9788206e4be6a9c51e23eba0d9bd458a167e70
pure-js <3690368+pure-js@users.noreply.github.com>
2019-02-16 00:49:56 +0300

    rewrite to functional component

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,5 @@
 // @flow
-import React, { Component } from 'react';
+import React from 'react';
 
 import bootstrap from 'bootstrap/dist/css/bootstrap.css';
 
@@ -12,65 +12,53 @@ type Props = {
   userAnswer: void,
 };
 
-class UserAnswer extends Component<Props> {
-  constructor(props: Props) {
-    super(props);
-    this.data = '';
-  }
+let data = '';
 
-  getValue = (value: string): void => {
-    this.data = value;
-  };
+const getValue = (value: string): void => {
+  data = value;
+};
 
-  handleSubmit = () => {
-    const { userAnswer } = this.props;
-    userAnswer(this.data);
-  };
-
-  render() {
-    return (
-      <form>
-        <div className={bootstrap['form-group']}>
-          <label className={styles['console-label']} htmlFor="console-output">
-            Web Console Output:
-          </label>
-          <div
-            className={styles.console_icon}
-          >
-            {'>'}
-          </div>
-          <Textarea getInputValue={this.getValue} />
-          <label>
-            Enter key starting a new line.
-            Please note that your input is case sensitive.
-          </label>
-        </div>
-        <div
-          className={`
-            ${bootstrap['form-group']}
-            ${bootstrap['btn-group']} 
-          `}
-        >
-          <button
-            id="answer"
-            onClick={this.handleSubmit}
-            type="button"
-            className={`${bootstrap.btn} ${bootstrap['btn-info']} ${button.btn_cursor}`}
-          >
-            Answer
-          </button>
-          <button
-            id="next-quiz"
-            onClick={this.handleSubmit}
-            type="button"
-            className={`${bootstrap.btn} ${bootstrap['btn-light']} ${button.btn_cursor}`}
-          >
-            {'I don\'t know'}
-          </button>
-        </div>
-      </form>
-    );
-  }
-}
+const UserAnswer = ({ userAnswer }: Props) => (
+  <form>
+    <div className={bootstrap['form-group']}>
+      <label className={styles['console-label']} htmlFor="console-output">
+        Web Console Output:
+      </label>
+      <div
+        className={styles.console_icon}
+      >
+        {'>'}
+      </div>
+      <Textarea getInputValue={getValue} />
+      <label>
+        Enter key starting a new line.
+        Please note that your input is case sensitive.
+      </label>
+    </div>
+    <div
+      className={`
+        ${bootstrap['form-group']}
+        ${bootstrap['btn-group']} 
+      `}
+    >
+      <button
+        id="answer"
+        onClick={() => userAnswer(data)}
+        type="button"
+        className={`${bootstrap.btn} ${bootstrap['btn-info']} ${button.btn_cursor}`}
+      >
+        Answer
+      </button>
+      <button
+        id="next-quiz"
+        onClick={() => userAnswer(data)}
+        type="button"
+        className={`${bootstrap.btn} ${bootstrap['btn-light']} ${button.btn_cursor}`}
+      >
+        {'I don\'t know'}
+      </button>
+    </div>
+  </form>
+);
 
 export default UserAnswer;
