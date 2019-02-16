24af1eeccf35816bf20799b6e1f0607653302efa
pure-js <3690368+pure-js@users.noreply.github.com>
2019-02-17 01:40:52 +0300

    add corect link to home

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,21 @@
 // @flow
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import bootstrap from 'bootstrap/dist/css/bootstrap.css';
 import styles from './Header.css';
 import grid from '../Grid/Grid.css';
 
 type Props = {
-  home: void,
   current: string,
   total: string,
 };
 
-const Header = ({ home, current, total }: Props) => (
+const Header = ({ current, total }: Props) => (
   <header>
     <div className={grid.container}>
       <nav className={`${bootstrap.navbar} ${styles['navbar_no-padding']}`}>
-        <a href="#" className={bootstrap['navbar-brand']} onClick={home}>JavaScript Quiz</a>
+        <Link to="/" className={bootstrap['navbar-brand']}>JavaScript Quiz</Link>
         <span className={bootstrap['navbar-text']}>
           {current}
           {' of '}
