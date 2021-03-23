801e19c6325bfc7b9440953743435235abd54f01
pure-js <3690368+pure-js@users.noreply.github.com>
2021-03-23 14:17:41 +0300

    fix: remove airbrake

--- src/components/ErrorBoundary.jsx
+++ src/components/ErrorBoundary.jsx
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
-// @ts-ignore
-import AirbrakeClient from 'airbrake-js';
 
 class ErrorBoundary extends Component {
   constructor(props) {
     super(props);
     this.state = { hasError: false };
-    this.airbrake = new AirbrakeClient({
-      projectId: 214923,
-      projectKey: 'ecb56343de55022bf8811d6097d63937',
-    });
   }
 
   componentDidCatch(error, info) {
     // Display fallback UI
     this.setState({ hasError: true });
-    // Send error to Airbrake
-    this.airbrake.notify({
-      error,
-      params: { info },
-    });
   }
 
   render() {
