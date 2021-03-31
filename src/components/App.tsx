4b41c3fecc4c3e817ea299c4762d78914a058847
pure-js <3690368+pure-js@users.noreply.github.com>
2021-04-01 01:23:30 +0300

    feat: add fallaback page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import React from 'react';
-import { Route } from 'wouter';
+import { Switch, Route } from 'wouter';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 import Home from '../scenes/Home/Home.tsx';
@@ -15,13 +15,18 @@ const defaultAnsers = {
 const App: React.FC = () => (
   <>
     <CssBaseline />
-    <Route path="/" component={Home} />
-    <Route path="/exam">
-      <Exam results={() => {}} />
-    </Route>
-    <Route path="/results">
-      <FinalResults userAnswers={defaultAnsers} />
-    </Route>
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route path="/exam">
+        <Exam results={() => {}} />
+      </Route>
+      <Route path="/results">
+        <FinalResults userAnswers={defaultAnsers} />
+      </Route>
+      <Route path="/:rest*">
+        {(params) => `404, Sorry the page ${params.rest} does not exist!`}
+      </Route>
+    </Switch>
   </>
 );
 
