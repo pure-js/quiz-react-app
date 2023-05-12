2f595fbbb72891837dd35923f4b5231cc87676a6
pure-js <3690368+pure-js@users.noreply.github.com>
2023-05-12 18:52:30 +0800

    build(deps): update dependencies including mui

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,33 +1,42 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import React from 'react';
 import { Switch, Route } from 'wouter';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import CssBaseline from '@mui/material/CssBaseline';
+import {
+  ThemeProvider,
+  StyledEngineProvider,
+  createTheme,
+} from '@mui/material/styles';
 
-import Home from '../scenes/Home/Home.tsx';
+import Home from '../scenes/Home/Home';
 import Exam from '../scenes/Exam/Exam';
-import FinalResults from '../scenes/FinalResults.tsx';
+import FinalResults from '../scenes/FinalResults';
 
 const defaultAnsers = {
   correct: 146,
   total: 100,
 };
 
+const theme = createTheme();
+
 const App: React.FC = () => (
-  <>
-    <CssBaseline />
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/exam">
-        <Exam results={() => {}} />
-      </Route>
-      <Route path="/results">
-        <FinalResults userAnswers={defaultAnsers} />
-      </Route>
-      <Route path="/:rest*">
-        {(params) => `404, Sorry the page ${params.rest} does not exist!`}
-      </Route>
-    </Switch>
-  </>
+  <StyledEngineProvider injectFirst>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/exam">
+          <Exam results={() => {}} />
+        </Route>
+        <Route path="/results">
+          <FinalResults userAnswers={defaultAnsers} />
+        </Route>
+        <Route path="/:rest*">
+          {(params) => `404, Sorry the page ${params.rest} does not exist!`}
+        </Route>
+      </Switch>
+    </ThemeProvider>
+  </StyledEngineProvider>
 );
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './components/App.tsx';
+import App from './components/App';
 
 const root = createRoot(document.getElementById('app'));
 root.render(<App />);
